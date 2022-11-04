935b8b42591ac872b70b51088065faa15670450e
cimu <cimu91894@qq.com>
2022-11-04 11:28:47 +0800

    完成了验证码输入框

---
+++ DAY38-VerifyAccount/script.js
@@ -0,0 +1,36 @@
+const codeItem = document.querySelectorAll('.item');
+
+codeItem[0].focus();
+
+
+
+codeItem.forEach((item, index) => {
+    item.addEventListener('keydown', function(e){
+        const val = e.key;
+        if(val >= 0 || val <= 9) {
+            setTimeout(() => {
+                item.value = val;
+                if(codeItem[index + 1]) {
+                    codeItem[index + 1].focus();
+                }
+            }, 10);
+        }else if(val === 'Backspace' && codeItem[index - 1]) {
+            setTimeout(() => {
+                codeItem[index - 1].focus();
+            }, 10)
+        }
+        else {
+            setTimeout(() => {
+                item.value = '';
+            }, 10)
+        }
+
+        setTimeout(() => {
+            if(item.value) {
+                item.classList.add('value');
+            } else {
+                item.classList.remove('value');
+            }
+        }, 20)
+    })
+})
